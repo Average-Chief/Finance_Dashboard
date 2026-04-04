e29f21473e4f563dc0e778607cf44e0a5e6fde4f
Average-Chief <achalsinghtomar3450@gmail.com>
2026-04-04 22:19:46 +0530

    Changed the structure and added bruno testing

--- app/config.py
+++ app/config.py
@@ -7,11 +7,4 @@ load_dotenv()
 secret_key = os.getenv("secret_key", "supersecretkey")
 algorithm = "HS256"
 access_token_expires_in = int(os.getenv("access_token_expires_in", 30))
-
-BASE_DIR = Path(__file__).resolve().parent  
-
-db_name = os.getenv("database_url", "finance.db").replace("sqlite:///", "")
-
-DB_PATH = BASE_DIR / db_name
-
-database_url = f"sqlite:///{DB_PATH}"
+database_url = os.getenv("database_url", "sqlite:///./finance.db")
